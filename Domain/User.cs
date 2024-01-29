ac4b63573f93aef8eac00164053708c68fbf6f84
EvpatiyKaloed <akshin-sasha@bk.ru>
2024-01-29 20:01:37 +0300

    Add project files.

---
+++ Domain/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class User
+    {
+        public string UserName { get; set; }  
+        public Guid  ID {  get; set; }
+        public int MoneyAmount { get; set; }
+        public List <Game> Games { get; set; }
+        public UserName (string username, Guid id , int moneyAmount, List games)
+        {
+            UserName= username;
+            ID = id;
+            MoneyAmount = moneyAmount;
+            Games = [];
+        }
+    }
+}
